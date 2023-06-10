b66c4084f225570614ab53d92532ce995a09d271
Jorge Fernandes <jorgefernandes.jms17@gmail.com>
2023-06-10 21:44:31 +0100

    new js

---
+++ JavaScript/aula11/exc008.js
@@ -0,0 +1,15 @@
+/* var vel = 50
+console.log(`A Velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!') */
+
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
+
