2202f54d83045073347e1735b112dfabc4d37c78
Jorge Fernandes <jorgefernandes.jms17@gmail.com>
2023-06-11 20:47:59 +0100

    new js

---
+++ JavaScript/aula12/ex011.js
@@ -0,0 +1,12 @@
+var idade = 123
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18){
+    console.log('Voto opcional')
+} else if (idade <= 100){
+    console.log('Voto obrigatório')
+} else if (idade > 100){
+    console.log('Fantasmas não votam')
+}
+
+
