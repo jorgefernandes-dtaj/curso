02c82c81e3725b0c32b94bb1150185752d39c3a6
Jorge Fernandes <jorgefernandes.jms17@gmail.com>
2023-06-14 19:33:21 +0100

    js

---
+++ JavaScript/aula14/for.js
@@ -0,0 +1,6 @@
+
+console.log('Vai começar...')
+for(var c = 1 ; c <= 5 ; c++) {
+    console.log(`Teste ${c}`)
+}
+console.log('FIM')
---
+++ JavaScript/aula14/node_1fcacc4312947.tmp.js
@@ -0,0 +1,13 @@
+
+
+/*
+var c = 1
+while (c <= 5){
+    console-log(c)
+    c++
+}
+*/
+
+for(var c = 1 ; c > 0 ; c++) {
+    console.log(`Teste ${c}`)
+}
